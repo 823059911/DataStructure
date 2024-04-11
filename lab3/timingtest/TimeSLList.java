c429adf8906ba5bee37b8a91248353eb4783cac4
Your Name <you@example.com>
2024-04-11 17:51:39 +0800

    new

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,23 +22,28 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        SLList<Integer> Ns= new SLList<>();
+        SLList<Integer> Ns = new SLList<>();
         SLList<Double> times = new SLList<>();
         SLList<Integer> opCounts = new SLList<>();
-        int[] sizes = {1000, 2000, 3000, 4000, 5000, 6000};
+        int[] sizes = {1000, 5000, 10000, 20000, 30000, 40000};
 
         for (int size : sizes) {
             SLList<Integer> tempList = new SLList<>();
             for (int i = 0; i <= size; i++) {
                 tempList.addLast(i);
             }
-            Stopwatch sw = new Stopwatch();
-            tempList.getLast(); // 在计时器开始和结束之间调用 getLast() 方法
-            double timeInSeconds = sw.elapsedTime();
-            Ns.addLast(size);
-            times.addLast(timeInSeconds);
-            opCounts.addLast(10000); // 这里应该记录 getLast() 方法的调用次数
+                int timeOfGetlast = 0;
+                Stopwatch sw = new Stopwatch();
+                tempList.getLast(); // 在计时器开始和结束之间调用 getLast() 方法
+                timeOfGetlast++;
+                double timeInSeconds = sw.elapsedTime();
+                Ns.addLast(size);
+                times.addLast(timeInSeconds);
+                opCounts.addLast(timeOfGetlast); // 这里应该记录 getLast() 方法的调用次数
+
+            }
+            printTimingTable(Ns, times, opCounts);
         }
-        printTimingTable(Ns, times, opCounts);
+
     }
-}
+
